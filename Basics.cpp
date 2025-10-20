ccc79fa83c26340cef4ca5c19b3160bb3b11a9cb
Saadi-Safdar-Pro <saadisafder@gmail.com>
2025-10-20 11:16:20 +0500

    Practiced DS in Lab

---
+++ Basics.cpp
@@ -0,0 +1,97 @@
+
+    // int age;
+    // int no;
+    // float  marks;
+    // char ch;
+    // bool b=true;
+    // string name;
+  
+    // cout<<"\nBytes in int are: "<<sizeof(int);
+    // cout<<"\nBytes in float are: "<<sizeof(float);
+    // cout<<"\nBytes in char are: "<<sizeof(char);
+    // cout<<"\nBytes in bool are: "<<sizeof(bool);
+    // cout<<"\nBytes in string are: "<<sizeof(string)<<endl;
+
+    // cout<<"\nName: ";cin>>name;
+    // cout<<"\nAge: ";cin>>age;
+    // cout<<"\nMarks: ";cin>>marks;
+    // cout<<"\nCharacter: ";cin>>ch;
+    // cout<<"\nBoolean: ";cin>>b;    
+
+    // cout<<"\nEnter a no to find: ";cin>>no;
+    // cout<<"\nSquare: "<< no * no;
+    // cout<<"\nCube: "<< no * no * no;
+    // cout<<"\nHalf: "<< no/2<<endl;
+
+    // int num;
+    // cout<<"\nEnter a num: ";
+    // cin>>num;
+    // if (num%2==0)
+    // {
+    //     cout<<"It is even.\n";
+    // }
+    // else
+    // {
+    //     cout<<"It is Odd.\n";
+    // }
+
+
+    // int arr[5];
+    // cout<<"\nEnter 5 integers: \n";
+    // for(int i=0; i<5; i++)
+    // {
+    // cout<<"Integer "<< i + 1 <<" : ";
+    // cin>>arr[i];
+    // }
+
+
+    // cout<<"\nYou entered: \n";
+    // for(int i=0; i<5; i++)
+    // {
+    // cout<<"Integer "<< i + 1 <<" : ";    
+    // cout<<arr[i]<< endl;
+    // }
+
+
+
+    // srand(time(0));
+    // int arr[100];
+    // for(int i=0; i<100; i++)
+    // {
+    //     arr[i] = rand() % 100;
+    // }
+    // for(int i=0; i<100; i++)
+    // {
+    //     cout<<"Random #: "<<arr[i]<<endl;
+    // }
+
+
+
+
+
+    // int zeroCount = 0; 
+    // int oddCount = 0;
+    // int evenCount = 0;
+    // int counter = 0;
+
+
+    // const int N = 10;
+
+    // void initialize(int& zeroCount, int& oddCount, int& evenCount);
+    // void getNumber(int& num);
+    // void classifyNumber(int zeroCount, int& oddCount, int& evenCount);
+    // void printResults(int zeroCount, int& oddCount, int& evenCount);
+
+
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+int main()
+{   
+
+    
+}
+
